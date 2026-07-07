1fa7bd16e980f604717dd7589dcf40ec2ce2139a
Wes <deir0006@algonquinlive.com>
2026-07-07 13:20:19 -0400

    Stubs for analytics engine. renamed analytics.h to analyticsengine.h

---
+++ include/AnalyticsEngine.h
@@ -0,0 +1,31 @@
+#ifndef EXPENSETRACKER_ANALYTICSENGINE_H
+#define EXPENSETRACKER_ANALYTICSENGINE_H
+
+// this template class calculates financial summaries
+template <typename T = double>
+class AnalyticsEngine {
+public:
+    T computeTotalExpenses() const {
+
+        return computeTotalExpenses(T{});
+    }
+
+    T computeTotalIncome() const {
+
+        return computeTotalIncomeT{};
+    }
+    T computeSavings() const {
+
+        return computeSavings(T{});
+    }
+    void computeCategorySpending() const {
+
+        computeCategorySpending(T{});
+    }
+    void computeBudgetUsage() const {
+
+        computeBudgetUsage(T{});
+    }
+};
+
+#endif //EXPENSETRACKER_ANALYTICSENGINE_H
