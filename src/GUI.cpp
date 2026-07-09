52f2eae33bd13157bf833e0fb0444cc93cebb55f
Wes Deir <deir.wesley@gmail.com>
2026-07-09 17:06:27 -0400

    feat: Added save/load from file inputs on main tab. Added search/filter fields for date, category and amount. added a reference to GUI.cpp.

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -11,6 +11,7 @@ GUI::GUI(QWidget* parent) : QWidget(parent), ui(new Ui::ExpenseTrackerWindow) {
     ui->inputDate->setMaximumDate(QDate::currentDate());
     ui->inputDate->setDate(QDate::currentDate());
 
+    // https://stackoverflow.com/questions/15686501/how-to-resize-qtablewidget-columns
     // stretch table columns evenly across the full table width
     ui->tableTransactions->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     ui->tableCategories->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
@@ -18,7 +19,6 @@ GUI::GUI(QWidget* parent) : QWidget(parent), ui(new Ui::ExpenseTrackerWindow) {
 
     // https://forum.qt.io/topic/139637/how-to-create-a-button-and-place-it-where-i-want-and-then-run-a-function-when-the-button-is-clicked-c-qt/3
     // connect(myButton, &QPushButton::clicked, this, &MyClass::myFunction);
-    // connect the button, signal, context and slot
     connect(ui->btnAddTransaction, &QPushButton::clicked, this, &GUI::onAddTransactionClicked);
     connect(ui->btnCancelTransaction, &QPushButton::clicked, this, &GUI::onCancelTransactionClicked);
     connect(ui->comboType, &QComboBox::currentTextChanged, this, &GUI::onTypeChanged);
