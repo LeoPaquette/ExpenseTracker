9b4be83533897e0cb30e0046d683d7adc9ecefd8
Wes Deir <deir.wesley@gmail.com>
2026-07-08 22:25:27 -0400

    feat: added date cap, connect() wiring, and the three slot bodies

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -5,9 +5,35 @@
 // builds the widget tree
 GUI::GUI(QWidget* parent) : QWidget(parent), ui(new Ui::ExpenseTrackerWindow) {
     ui->setupUi(this);
+
+    // dont allow future dated transactions
+    ui->inputDate->setMaximumDate(QDate::currentDate());
+    ui->inputDate->setDate(QDate::currentDate());
+
+    // https://forum.qt.io/topic/139637/how-to-create-a-button-and-place-it-where-i-want-and-then-run-a-function-when-the-button-is-clicked-c-qt/3
+    // connect(myButton, &QPushButton::clicked, this, &MyClass::myFunction);
+    // connect the button, signal, context and slot
+    connect(ui->btnAddTransaction, &QPushButton::clicked, this, &GUI::onAddTransactionClicked);
+    connect(ui->btnCancelTransaction, &QPushButton::clicked, this, &GUI::onCancelTransactionClicked);
+    connect(ui->comboType, &QComboBox::currentTextChanged, this, &GUI::onTypeChanged);
 }
 
 // free ui class mem
 GUI::~GUI() {
     delete ui;
+}
+
+// jumps to the transaction form tab
+void GUI::onAddTransactionClicked() {
+    ui->tabWidget->setCurrentWidget(ui->Tab_TransactionForm);
+}
+
+// returns to the main tab without saving
+void GUI::onCancelTransactionClicked() {
+    ui->tabWidget->setCurrentWidget(ui->Tab_Main);
+}
+
+// relabels the extra field to match the selected transaction type
+void GUI::onTypeChanged(const QString& type) {
+    ui->labelExtra->setText(type == "Income" ? "Source:" : "Payment Method:");
 }
